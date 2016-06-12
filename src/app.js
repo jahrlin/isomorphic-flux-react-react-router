9e682e5bc279a97d8eb0a02ca6158764651bdeb9
Joakim Ahrlin <joakim.ahrlin@gmail.com>
2016-06-12 20:27:51 +0200

    place routes in a separate file

--- src/app.js
+++ src/app.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Routes from './routes'
 import {browserHistory, Router, Route, IndexRoute} from 'react-router'
-import RootLayout from 'containers/RootLayout/RootLayout'
-import Home from 'containers/Home/Home'
-import Subroute from 'containers/Subroute/Subroute'
-import Parameterized from 'containers/Parameterized/Parameterized'
 
 const mountNode = document.querySelector('#root');
 ReactDOM.render(
-    <Router history={browserHistory}>
-        <Route path="/" component={RootLayout}>
-            <IndexRoute component={Home} />
-            <Route path="subroute/" component={Subroute} />
-            <Route path="parameterized/:param" component={Parameterized} />
-        </Route>
-    </Router>, mountNode
+    Routes, mountNode
 );
