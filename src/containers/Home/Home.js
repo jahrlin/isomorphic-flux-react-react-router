947cb037e685444ec18c6dd48f12d4a43b60138e
Jocke <joakim.ahrlin@gmail.com>
2016-06-19 15:09:49 +0200

    add fa icons

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -8,12 +8,15 @@ class Home extends React.Component {
         <h2>This is the &lt;Home&gt; component</h2>
         <ul className="links">
           <li className="links__item">
+            <i className="fa fa-check" aria-hidden="true"></i>
             <Link to="subroute/">A subroute</Link>
           </li>
           <li className="links__item">
+            <i className="fa fa-check" aria-hidden="true"></i>
             <Link to="parameters/value/">Parameters</Link>
           </li>
           <li className="links__item">
+            <i className="fa fa-check" aria-hidden="true"></i>
             <Link to="asyncdata/">A route that fetches data asynchronuously</Link>
           </li>
         </ul>
